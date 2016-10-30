d45c8a4cdb38526c894f63b1b4b71ae87d845148
ImNatho <me@natho.io>
2016-10-30 15:44:15 +1030

    create paste model.

---
+++ models/paste.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Paste = new Schema({
+    key: { type: String, index: true },
+    data: String,
+    data_type: { type: String, default: 'text' },
+    creator: {
+        name: { type: String, default: 'Unknown' },
+        user_agent: {
+            platform: String,
+            browser: String,
+            version: String
+        }
+    }
+    meta: {
+        created_on: { type: Date, default: Date.now },
+        updated_at: { type: Date, default: Date.now }
+    }
+});
+
+module.exports = mongoose.model('Paste', Paste);
