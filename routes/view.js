2db35c837766e19217c56aed72fc1c735c730b2d
ImNatho <me@natho.io>
2016-11-08 00:54:10 +1030

    Start of view route.
    ToDo:
    Templates
    Other DB stuff.

--- routes/view.js
+++ routes/view.js
@@ -1,23 +1,43 @@
 var Router = require('express').Router({mergeParams: true});
 
+/* Get paste object and store in req object. */
 Router.use(function(req, res, next) {
-    Paste.find().limit(1).exec(function(err, data) {
-        if(data.length != 1) { next(); return; }
+    Paste.find({ key: req.params.key }).limit(1).exec(function(err, data) {
+        if(data.length != 1) {
+            res.status(404);
+            res.send('There was no paste found with the ID of "' + req.params.key + '"!');
+            return;
+        }
+
+        var paste = data[0];
+        if(paste.removed.removed) {
+            res.status(410);
+            res.send('Paste has been removed by an administrator. Reason: ' + paste.removed.reason);
+            return;
+        }
+        req.paste = paste;
     })
 });
 
+/* Retrieve paste request */
 Router.get('/', function(req, res) {
-    res.send('get paste ' + req.params.paste);
+    res.send('Get paste ' + req.paste.key);
 });
 
 /* Alternate JSON response */
 Router.get('/json', function(req, res) {
-    res.json({ paste: req.params.paste });
+    res.json({
+        key: req.paste.key,
+        type: req.paste.data_type,
+        paste: req.paste.data,
+        creator: req.paste.creator.name,
+        created_on: req.paste.meta.created_on
+    });
 });
 
 /* Alternate raw response */
 Router.get('/raw', function(req, res) {
-    res.send('Get raw paste: ' + req.params.paste);
+    res.send('Get raw paste: ' + req.paste.key);
 });
 
 module.exports = Router;
